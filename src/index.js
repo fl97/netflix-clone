17649067596ea75f1fa027cb9ff385447ce43cab
fl97 <50992060+fl97@users.noreply.github.com>
2020-10-20 15:15:42 +0200

    connected/seed firebase database

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import 'normalize.css'
 import App from './app';
 import {GlobalStyles} from './global-styles'
 
+import {firebase} from './lib/firebase.prod'
+import {FirebaseContext} from './context/firebase'
+
 ReactDOM.render(
   <React.StrictMode>
-    <React.Fragment>
+    <FirebaseContext.Provider value={{firebase}}>
       <GlobalStyles />
       <App />
-    </React.Fragment>
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,7 +2,16 @@ import Firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const config = {}
+const config = {
+    apiKey: "AIzaSyC9u0DmsgK0ouMXpT8AmGFMt-eBW2CgvGU",
+    authDomain: "netflix-clone-792d1.firebaseapp.com",
+    databaseURL: "https://netflix-clone-792d1.firebaseio.com",
+    projectId: "netflix-clone-792d1",
+    storageBucket: "netflix-clone-792d1.appspot.com",
+    messagingSenderId: "967521821761",
+    appId: "1:967521821761:web:4d28222ee72785385952c0",
+    measurementId: "G-ND9E8LFG9L"
+}
 
 const firebase = Firebase.initializeApp(config)
 
