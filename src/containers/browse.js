4b0360147f1aeda1f464d8404d2c4e0564777a96
fl97 <50992060+fl97@users.noreply.github.com>
2020-10-22 14:59:19 +0200

    added Profile Selection View

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+
+import { FirebaseContext } from '../context/firebase'
+import { SelectProfileContainer } from './profiles'
 
 export function BrowseContainer({slides}) {
-    return (
-        <p>Hello</p>
+
+    const [profile, setProfile] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const { firebase } = useContext(FirebaseContext)
+    const user = firebase.auth().currentUser || {}
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [profile.displayName])
+
+    return profile.displayName ? (
+        loading ? (
+            <Loading src={user.photoURL} />
+        ) : null
+    ) : (
+        <SelectProfileContainer user = { user } setProfile = { setProfile } />
     )
 };
